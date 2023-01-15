7e6c335600feb09a3a83e409a3b86d230eacdb49
Maycon Silva <mayconslv.contato@gmail.com>
2023-01-15 16:45:01 -0300

    estilização da página History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,65 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export function History() {
   return (
-    <>
-      <h1>History</h1>
-    </>
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>28 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>28 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>28 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>28 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>28 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>28 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
