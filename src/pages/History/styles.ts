efb7dcb6d7d4d2d794d8b47519c99cffa448d680
Maycon Silva <mayconslv.contato@gmail.com>
2023-01-16 00:51:49 -0300

    status color na history page

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -60,3 +60,27 @@ export const HistoryList = styled.div`
     }
   }
 `
+
+const STATUS_COLOR = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLOR
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: .8rem;
+
+  &::before {
+    content: '';
+    width: .8rem;
+    height: .8rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLOR[props.statusColor]]};
+  }
+`
