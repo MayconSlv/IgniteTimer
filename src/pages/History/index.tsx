6ce3fdaa4a071316f35e1c5a2da1f3c798f88a1c
Maycon Silva <mayconslv.contato@gmail.com>
2023-01-23 16:21:57 -0300

    adicionando ciclos no histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,17 +1,15 @@
-import { useContext } from "react";
-import { HistoryContainer, HistoryList, Status } from "./styles";
-import { CyclesContext } from "../../contexts/CyclesContext";
+import { useContext } from "react"
+import { HistoryContainer, HistoryList, Status } from "./styles"
+import { CyclesContext } from "../../contexts/CyclesContext"
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
 
   return (
     <HistoryContainer>
-      <h1>Meu Histórico</h1>
-
-      <pre>
-        {JSON.stringify(cycles, null, 2)}
-      </pre>
+      <h1>Meu Histórico</h1>    
 
       <HistoryList>
         <table>
@@ -24,59 +22,32 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Ha 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
+            {cycles.map(cycle => {
+              return (
+                <tr>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>{formatDistanceToNow(cycle.startDate,{
+                    addSuffix: true,
+                    locale: ptBR
+                  })}</td>
+                  <td>
+                    { cycle.finishedDate && (
+                      <Status statusColor="green">Concluído</Status>
+                      )}
+                      
+                    { cycle.interruptedDate && (
+                      <Status statusColor="red">Interrompido</Status>
+                      )}
 
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Ha 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
+                    { !cycle.interruptedDate && !cycle.finishedDate && (
+                      <Status statusColor="yellow">Em andmento</Status> 
+                    )}
 
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Ha 2 meses</td>
-              <td>
-                <Status statusColor="red">Interrompido</Status>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Ha 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Ha 2 meses</td>
-              <td>
-                <Status statusColor="yellow">Em andamento</Status>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Tarefa</td>
-              <td>28 minutos</td>
-              <td>Ha 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </HistoryList>
